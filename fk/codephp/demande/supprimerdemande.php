350e42d8d20a241b5e4586b48ee0ea3319fc04c1
Rayman1200 <kingfaboraymond@gmail.com>
2026-06-10 21:36:10 +0100

    Refactor demande deletion process in supprimercompte.php

---
+++ fk/codephp/demande/supprimerdemande.php
@@ -0,0 +1,15 @@
+<?php
+
+require('../connexionBD.php');
+
+// Récupère les données du formulaire
+$id = $_SESSION['id'] ?? '';
+
+// Insertion
+$stmt = $conn->prepare("DELETE FROM demande WHERE ID= ? ");
+$stmt->bind_param("s", $id);
+$success = $stmt->execute();
+if (!$success) {
+    echo '<div style="...">❌ Une erreur s\'est produite</div>';
+}
+?>
